3466a75f42e424423b4458fc5d5c07cc71a4707c
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-19 21:40:44 -0400

    connect to rabitmq

---
+++ consumer.py
@@ -0,0 +1,23 @@
+from decouple import config
+import pika
+
+
+
+URL = config("URL")
+params = pika.URLParameters(url=URL)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='main')
+
+def callback(ch, method, properties, body):
+    print("-"*50)
+    print('Received in main/consumer')
+    print(body)
+    print("-"*50)
+
+
+channel.basic_consume(
+    queue='main', on_message_callback=callback)
+print('Started Consuming Main')
+channel.start_consuming()
+channel.close()
