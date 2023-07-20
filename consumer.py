d9dc169e3892a7190da7e8141f009a8761969391
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-19 23:30:27 -0400

    internal http request

--- consumer.py
+++ consumer.py
@@ -1,7 +1,7 @@
 from decouple import config
 import pika
-
-
+import json
+from app.models.product import Product
 
 URL = config("URL")
 params = pika.URLParameters(url=URL)
@@ -9,11 +9,28 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.queue_declare(queue='main')
 
+
 def callback(ch, method, properties, body):
-    print("-"*50)
-    print('Received in main/consumer')
-    print(body)
-    print("-"*50)
+    data = json.loads(body)
+    print('Received in main: ', data, '\n')
+    if properties.content_type == 'product_created':
+        product = Product(**data)
+        product.save()
+        print('Product created')
+    elif properties.content_type == 'product_updated':
+        product = Product.objects.get(id=data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        product.save()
+        print('Product updated')
+    elif properties.content_type == 'product_deleted':
+        product = Product.objects.get(id=data)
+        product.delete()
+        print('Product deleted')
+    elif properties.content_type == 'product_viewed':
+        print('Product viewed')
+    else:
+        print('No action for this type')
 
 
 channel.basic_consume(
