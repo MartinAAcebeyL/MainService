0d1fa2082b38aa39367fa3f9efd1bb6cf464ee2d
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-17 20:52:31 -0400

    add producer

---
+++ producer.py
@@ -0,0 +1,13 @@
+import pika
+import json
+from decouple import config
+
+params = pika.URLParameters(config('URL'))
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(
+        routing_key='admin', body=json.dumps(body), properties=properties)
