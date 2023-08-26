082eb8496ffd057e44f27a52fa3e454555a6ace3
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-26 00:12:39 -0400

    resolve mistakes at  producer

--- producer.py
+++ producer.py
@@ -9,5 +9,5 @@ channel = connection.channel()
 
 def publish(method, body):
     properties = pika.BasicProperties(method)
-    channel.basic_publish(
-        routing_key='admin', body=json.dumps(body), properties=properties)
+    channel.basic_publish(exchange='',
+                          routing_key='admin', body=json.dumps(body), properties=properties)
