fe84b00de2e164f058f34656633176515f53bffb
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-26 00:12:10 -0400

    update consumer.py

--- consumer.py
+++ consumer.py
@@ -13,7 +13,7 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     data = json.loads(body)
-    print('Received in main: ', data, '\n')
+    print('Received in main data: ', data)
     if properties.content_type == 'product_created':
         product = Product(title=data['title'], image=product['image'])
         product.save(get_session())
@@ -38,4 +38,4 @@ channel.basic_consume(
     queue='main', on_message_callback=callback, auto_ack=True)
 print('Started Consuming Main')
 channel.start_consuming()
-channel.close()
+# channel.close()
